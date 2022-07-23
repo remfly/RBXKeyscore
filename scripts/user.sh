f2c90e92f9cc58fecb88966bea4de0f93411805f
remfly <remfly@pm.me>
2022-07-23 12:29:38 -0300

    Add friends, followings and followers to search results

--- scripts/user.sh
+++ scripts/user.sh
@@ -10,7 +10,7 @@ then
     exit
 fi
 
-version="$(cat ./version)"
+version="$(cat ../version)"
 
 echo -e "\n|--------------- RBXKeyscore v${version} ---------------|\n\n"
 
@@ -20,9 +20,13 @@ echo -e "\nFetching..."
 
 uid="$(curl -s https://api.roblox.com/users/get-by-username?username=${name} | jq '.Id')"
 
-data="$(curl -s https://users.roblox.com/v1/users/${uid})"
+profile="$(curl -s https://users.roblox.com/v1/users/${uid})"
+username_history="$(curl -s https://users.roblox.com/v1/users/${uid}/username-history)"
+friends="$(curl -s https://friends.roblox.com/v1/users/${uid}/friends/count)"
+followings="$(curl -s https://friends.roblox.com/v1/users/${uid}/followings/count)"
+followers="$(curl -s https://friends.roblox.com/v1/users/${uid}/followers/count)"
 
-if [ "$(echo ${data} | jq '.name' | tr --delete '\"')" == "null" ]
+if [ "$(echo ${profile} | jq '.name' | tr --delete '\"')" == "null" ]
 then
     echo -e "Failed!\n\nNo users with this name were found. Check your spelling and try again!\n"
     exit
@@ -34,11 +38,14 @@ cat << EOF
 
 --- $(date +%m/%d/%Y) at $(date +%T) [UTC: $(date +%s)] ---
 
-Username: $(echo ${data} | jq ".name" | tr --delete '\"')
-Display Name: $(echo ${data} | jq ".displayName" | tr --delete '\"')
-Description: $(echo ${data} | jq '.description' | tr --delete '\"')
-Creation date: $(echo ${data} | jq ".created" | tr --delete '\"')
-Past usernames: $(curl -s https://users.roblox.com/v1/users/${uid}/username-history | jq '.data[] | .[]' | tr --delete '\"'  | tr '\n' ' ')
+Username: $(echo ${profile} | jq ".name" | tr --delete '\"')
+Display Name: $(echo ${profile} | jq ".displayName" | tr --delete '\"')
+Description: $(echo ${profile} | jq '.description' | tr --delete '\"')
+Creation date: $(echo ${profile} | jq ".created" | tr --delete '\"')
+Friends: $(echo ${friends} | jq '.count')
+Following: $(echo ${followings} | jq '.count')
+Followers: $(echo ${followers} | jq '.count')
+Past usernames: $(echo ${username_history} | jq '.data[] | .[]' | tr --delete '\"'  | tr '\n' ' ')
 Profile: https://roblox.com/users/${uid}/profile
 
 EOF
